9831db612b52f837c80d2c97b1b3b7e8aacabb5f
Piotrek38 <piotr.klamka@gmail.com>
2016-03-02 11:13:57 +0100

    Story: Naprawic blad zwiazany z nowymi danymi NewData.txt Nowy test unitowy dla poprawionego dekodera. Reviewer: Kuba

--- test/virtual/friend/FriendDataCoderTest.java
+++ test/virtual/friend/FriendDataCoderTest.java
@@ -12,19 +12,20 @@ import org.junit.Test;
 
 public class FriendDataCoderTest {
 
+	//ExampleDataBase64.txt has data in specific order, uses C U Q P prefixes
+	//NewData.txt has data in not specific order, uses A C P S prefixes
 	
-	public String dataCoder() throws IOException {
-		
+	private String getCodedDataWithSpecificOrder() throws IOException {
 		List<String> quotes = Files.readAllLines(Paths.get("exampleData\\quotes.txt"));
 		List<String> questions = Files.readAllLines(Paths.get("exampleData\\questions.txt"));
 		List<String> sciencequestions = Files.readAllLines(Paths.get("exampleData\\sciencequestions.txt"));
 		List<String> puzzles = Files.readAllLines(Paths.get("exampleData\\puzzles.txt"));
-		
-		
+
 		String resultBeforeCoding = "";
 
 		for (int i = 0; i < quotes.size(); i++) {
-			resultBeforeCoding = resultBeforeCoding + "C" + quotes.get(i) + "#" + 
+			resultBeforeCoding = resultBeforeCoding +
+					"C" + quotes.get(i) + "#" +
 					"A" + questions.get(i) + "#" +
 					"S" + sciencequestions.get(i)+ "#" +
 					"P" + puzzles.get(i) + "#";
@@ -33,10 +34,12 @@ public class FriendDataCoderTest {
 		String resultAfterCoding = Base64.getEncoder().encodeToString(resultBeforeCoding.getBytes());
 		return resultAfterCoding;
 	}
-	
+
+
+
 	@Test
-	public void testDecodeString() throws IOException{
-		String codedData = dataCoder();
+	public void testDecodeStringWithSpecifiOrder() throws IOException{
+		String codedData = getCodedDataWithSpecificOrder();
 		FriendDataDecoder friendDataCoder = new FriendDataDecoder();
 		friendDataCoder.getData(codedData);
 		
@@ -52,6 +55,49 @@ public class FriendDataCoderTest {
 		Assert.assertEquals("A man has a wolf, a goat, and a cabbage. He must cross a river with the two animals and the cabbage. There is a small rowing boat, in which he can take only one thing with him at a time. If, however, the wolf and the goat are left alone, the wolf will eat the goat. If the goat and the cabbage are left alone, the goat will eat the cabbage. How can the man get across the river with the two animals and the cabbage?", friendDataCoder.puzzles.get(0)[0]);
 		Assert.assertEquals("Don't know", friendDataCoder.puzzles.get(0)[1]);
 
+	}
+
+	private String getCodedDataWithAllPrefixesWithoutSpecificOrder() throws IOException {
+		List<String> quotes = Files.readAllLines(Paths.get("exampleData\\quotes.txt"));
+		List<String> questions = Files.readAllLines(Paths.get("exampleData\\questions.txt"));
+		List<String> scienceQuestions = Files.readAllLines(Paths.get("exampleData\\sciencequestions.txt"));
+		List<String> puzzles = Files.readAllLines(Paths.get("exampleData\\puzzles.txt"));
+
+		String resultBeforeCoding = "";
+
+		for (String data : quotes) {
+			resultBeforeCoding = resultBeforeCoding + "C" + data + "#";
+		}
+		for (String data : questions) {
+			resultBeforeCoding = resultBeforeCoding + "U" + data + "#";
+		}
+		for (String data : questions) {
+			resultBeforeCoding = resultBeforeCoding + "A" + data + "#";
+		}
+		for (String data : puzzles) {
+			resultBeforeCoding = resultBeforeCoding + "P" + data + "#";
+		}
+		for (String data : scienceQuestions) {
+			resultBeforeCoding = resultBeforeCoding + "Q" + data + "#";
+		}
+		for (String data : scienceQuestions) {
+			resultBeforeCoding = resultBeforeCoding + "S" + data + "#";
+		}
+
+		String resultAfterCoding = Base64.getEncoder().encodeToString(resultBeforeCoding.getBytes());
+		return resultAfterCoding;
+	}
+
+	@Test
+	public void testDecodeStringBasedOnPrefixes() throws IOException {
+		String codedData = getCodedDataWithAllPrefixesWithoutSpecificOrder();
+		FriendDataDecoder friendDataCoder = new FriendDataDecoder();
+		friendDataCoder.getData(codedData);
+
+		Assert.assertEquals(4, friendDataCoder.quotes.size());
+		Assert.assertEquals(4, friendDataCoder.puzzles.size());
+		Assert.assertEquals(8, friendDataCoder.scienceTest.size());
+		Assert.assertEquals(8, friendDataCoder.questions.size());
 	}
 	
 }
