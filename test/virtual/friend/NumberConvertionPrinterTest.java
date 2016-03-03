2d63462bea298d0b55a887f578adca046cce8257
rafal-p <rafal.pajak@interia.pl>
2016-03-03 12:57:45 +0100

    Refactor of tests for conversion game

--- test/virtual/friend/NumberConvertionPrinterTest.java
+++ test/virtual/friend/NumberConvertionPrinterTest.java
@@ -16,18 +16,20 @@ public class NumberConvertionPrinterTest  {
     SystemConsoleStub stub;
     RandomNumberStub numberGenerator;
     NumberConvertionPrinter numberConvertionPrinter;
+    private static final int RANDOM_NUMBER = 23;
+    private static final String RANDOM_NUMBER_BINARY = "10111";
 
     @Before
     public void before(){
         stub = new SystemConsoleStub();
-        numberGenerator = new RandomNumberStub(23);
+        numberGenerator = new RandomNumberStub(RANDOM_NUMBER);
         numberConvertionPrinter = new NumberConvertionPrinter(stub, numberGenerator);
     }
 
     @Test
     public void generateRandomNumber() {
         int result = numberConvertionPrinter.generateRandomNumber();
-        Assert.assertEquals(result, 23);
+        Assert.assertEquals(result, RANDOM_NUMBER);
     }
 
     @Test
@@ -36,7 +38,7 @@ public class NumberConvertionPrinterTest  {
         int resultNumber = numberConvertionPrinter.generateRandomNumber();
         String result = numberConvertionPrinter.convertDecimalToBinary(resultNumber);
 
-        Assert.assertEquals(result, "10111");
+        Assert.assertEquals(result, RANDOM_NUMBER_BINARY);
     }
 
     @Test
@@ -44,17 +46,17 @@ public class NumberConvertionPrinterTest  {
 
         stub.addUserAction("17");
         numberConvertionPrinter.next();
-        Assert.assertEquals("Skonwertuj liczbe binarna 10111 do postaci dziesietnej", stub.getProgramResponseAtt(0));
+        Assert.assertEquals("Skonwertuj liczbe binarna " + RANDOM_NUMBER_BINARY + " do postaci dziesietnej", stub.getProgramResponseAtt(0));
         Assert.assertEquals("Zla odpowiedz", stub.getProgramResponseAtt(1));
-        Assert.assertEquals("Poprawna odpowiedz to 23", stub.getProgramResponseAtt(2));
+        Assert.assertEquals("Poprawna odpowiedz to " + RANDOM_NUMBER, stub.getProgramResponseAtt(2));
     }
 
     @Test
     public void shouldShowRandomBinaryNumberCorrectAnswer(){
 
-        stub.addUserAction("23");
+        stub.addUserAction("" + RANDOM_NUMBER);
         numberConvertionPrinter.next();
-        Assert.assertEquals("Skonwertuj liczbe binarna 10111 do postaci dziesietnej", stub.getProgramResponseAtt(0));
+        Assert.assertEquals("Skonwertuj liczbe binarna " + RANDOM_NUMBER_BINARY + " do postaci dziesietnej", stub.getProgramResponseAtt(0));
         Assert.assertEquals("Gratulacje, poprawna odpowiedz!", stub.getProgramResponseAtt(1));
     }
 }
